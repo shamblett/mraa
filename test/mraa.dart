f893bcb12716574421180243bea3b359330779cd
Steve Hamblett <steve.hamblett@linux.com>
2019-10-03 14:52:47 +0100

    Tests

--- test/mraa.dart
+++ test/mraa.dart
@@ -6,11 +6,17 @@
  */
 
 import 'package:mraa/mraa.dart';
+import 'package:test/test.dart';
 
 int main() {
-  final Mraa mraa = Mraa();
-  final String mraaVersion = mraa.version();
-  print(mraaVersion);
+  group('Common', () {
+    test('Version', () {
+      final Mraa mraa = Mraa();
+      final String mraaVersion = mraa.version();
+      print('Version is : $mraaVersion');
+      expect(mraaVersion, 'v2.0.0-37-g0a12c5a');
+    });
+  });
 
   return 0;
 }
