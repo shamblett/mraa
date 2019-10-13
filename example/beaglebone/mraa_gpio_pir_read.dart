4d5947499a8ac8ae88d44d740f4e5d251c5a8cd6
Steve Hamblett <steve.hamblett@linux.com>
2019-10-13 11:58:06 +0100

    Beagle examples

--- example/beaglebone/mraa_gpio_pir_read.dart
+++ example/beaglebone/mraa_gpio_pir_read.dart
@@ -10,7 +10,7 @@ import 'dart:io';
 import 'package:mraa/mraa.dart';
 
 // The GPIO pin for the PIR motion sensor, set as needed.
-const int lightSensorGPIOPin = 73;
+const int pirSensorGPIOPin = 73;
 
 /// Check if the PIR motion sensor has been triggered
 int main() {
@@ -37,23 +37,16 @@ int main() {
   /// The PIR sensor initialisation
   print('Initialising AIO');
   final ffi.Pointer<MraaGpioContext> context =
-      mraa.gpio.initialise(lightSensorGPIOPin);
-
-  /// Set the pin direction
-  ret = mraa.gpio.direction(context, MraaGpioDirection.mraaGpioIn);
-  if (ret != MraaReturnCodes.mraaSuccess) {
-    print(
-        'Beaglebone Green - failed to set GPIO pin $lightSensorGPIOPin direction, exiting');
-    exit(-1);
-  }
+      mraa.gpio.initialise(pirSensorGPIOPin);
 
   print('Checking for a PIR motion trigger');
-  for (int i = 1; i <= 100; i++) {
-    final int val = mraa.gpio.read(context);
+  for (int i = 1; i <= 10000; i++) {
+    int val = mraa.gpio.read(context);
     if (val == 1) {
       print('PIR motion sensor has triggered');
     }
-    sleep(const Duration(milliseconds: 100));
+    val = 0;
+    sleep(const Duration(milliseconds: 300));
   }
 
   return 0;
