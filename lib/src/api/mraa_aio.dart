83d7c926ce9f2acafc4053d45aca98e6cade2614
Steve Hamblett <steve.hamblett@linux.com>
2019-10-31 09:05:34 +0000

    Documentation updates

--- lib/src/api/mraa_aio.dart
+++ lib/src/api/mraa_aio.dart
@@ -7,28 +7,31 @@
 
 // ignore_for_file: avoid_positional_boolean_parameters
 
+// ignore_for_file: avoid_private_typedef_functions
+
 part of mraa;
 
-/// C Function type typedefs
-typedef returnMraaAioContextIntParameterFunc = Pointer<MraaAioContext> Function(
+/// C Function signature typedefs
+typedef _returnMraaAioContextIntParameterFunc = Pointer<MraaAioContext> Function(
     Int32);
-typedef returnIntMraaAioContextParameterFunc = Int32 Function(
+typedef _returnIntMraaAioContextParameterFunc = Int32 Function(
     Pointer<MraaAioContext>);
-typedef returnDoubleMraaAioContextParameterFunc = Double Function(
+typedef _returnFloatMraaAioContextParameterFunc = Float Function(
     Pointer<MraaAioContext>);
-typedef returnIntMraaAioContextIntParameterFunc = Int32 Function(
+typedef _returnIntMraaAioContextIntParameterFunc = Int32 Function(
     Pointer<MraaAioContext>, Int32);
 
 /// Dart Function typedefs
-typedef MraaAioInitialiseType = Pointer<MraaAioContext> Function(int);
-typedef MraaAioReadType = int Function(Pointer<MraaAioContext>);
-typedef MraaAioReadDoubleType = double Function(Pointer<MraaAioContext>);
-typedef MraaAioCloseType = int Function(Pointer<MraaAioContext>);
-typedef MraaAioSetBitType = int Function(Pointer<MraaAioContext>, int);
-typedef MraaAioGetBitType = int Function(Pointer<MraaAioContext>);
+typedef _MraaAioInitialiseType = Pointer<MraaAioContext> Function(int);
+typedef _MraaAioReadType = int Function(Pointer<MraaAioContext>);
+typedef _MraaAioReadDoubleType = double Function(Pointer<MraaAioContext>);
+typedef _MraaAioCloseType = int Function(Pointer<MraaAioContext>);
+typedef _MraaAioSetBitType = int Function(Pointer<MraaAioContext>, int);
+typedef _MraaAioGetBitType = int Function(Pointer<MraaAioContext>);
 
 /// The AIO MRAA API
-/// AIO is the analogue input & output interface to libmraa.
+///
+/// AIO is the analogue input & output interface to MRAA
 /// It is used to read or set the voltage applied to an AIO pin.
 class MraaAio {
   /// Construction
@@ -43,83 +46,87 @@ class MraaAio {
   bool _noJsonLoading = false;
 
   /// C Pointers
-  Pointer<NativeFunction<returnMraaAioContextIntParameterFunc>> _initPointer;
-  Pointer<NativeFunction<returnIntMraaAioContextParameterFunc>> _readPointer;
-  Pointer<NativeFunction<returnDoubleMraaAioContextParameterFunc>>
+  Pointer<NativeFunction<_returnMraaAioContextIntParameterFunc>> _initPointer;
+  Pointer<NativeFunction<_returnIntMraaAioContextParameterFunc>> _readPointer;
+  Pointer<NativeFunction<_returnFloatMraaAioContextParameterFunc>>
       _readDoublePointer;
-  Pointer<NativeFunction<returnIntMraaAioContextParameterFunc>> _closePointer;
-  Pointer<NativeFunction<returnIntMraaAioContextIntParameterFunc>>
+  Pointer<NativeFunction<_returnIntMraaAioContextParameterFunc>> _closePointer;
+  Pointer<NativeFunction<_returnIntMraaAioContextIntParameterFunc>>
       _setBitPointer;
-  Pointer<NativeFunction<returnIntMraaAioContextParameterFunc>> _getBitPointer;
+  Pointer<NativeFunction<_returnIntMraaAioContextParameterFunc>> _getBitPointer;
 
   /// Dart Functions
-  MraaAioInitialiseType _initFunc;
-  MraaAioReadType _readFunc;
-  MraaAioReadDoubleType _readDoubleFunc;
-  MraaAioCloseType _closeFunc;
-  MraaAioSetBitType _setBitFunc;
-  MraaAioGetBitType _getBitFunc;
+  _MraaAioInitialiseType _initFunc;
+  _MraaAioReadType _readFunc;
+  _MraaAioReadDoubleType _readDoubleFunc;
+  _MraaAioCloseType _closeFunc;
+  _MraaAioSetBitType _setBitFunc;
+  _MraaAioGetBitType _getBitFunc;
 
   /// Initialise - mraa_aio_init
-  /// Initialise an Analog input device, connected to the specified pin.
-  /// Aio pins are always 0 indexed reguardless of their position.
+  ///
+  /// Initialise an analogue input device connected to the specified pin.
+  /// AIO pins are always 0 indexed regardless of their position.
   /// Check your board mapping for details.
-  /// An arduino style layout will have A0 as pin14 but AIO0.
   Pointer<MraaAioContext> initialise(int pin) => _initFunc(pin);
 
   /// Read - mraa_aio_read_float
-  /// Read the input voltage and return it as a normalized float (0.0f-1.0f).
-  /// Returns The current input voltage or -1.0 for error
+  ///
+  /// Read the input voltage and return it as a normalized double (0.0 - 1.0).
+  /// Returns The current input voltage or [Mraa.aioReadDoubleError] on error.
   double readDouble(Pointer<MraaAioContext> dev) => _readDoubleFunc(dev);
 
   /// Read - mraa_aio_read
-  /// Read the input voltage. By default mraa will shift the raw value
+  ///
+  /// Read the input voltage. By default MRAA will shift the raw value
   /// up or down to a 10 bit value.
-  /// Returns The current input voltage or -1 for error
+  /// Returns the current input voltage or [Mraa.aioReadError] for error
   int read(Pointer<MraaAioContext> dev) => _readFunc(dev);
 
   /// Close - mraa_aio_close
-  /// Close the analog input context, this will free the memory for the context.
-  /// MRAA_SUCCESS indicates correct closure
+  ///
+  /// Close the analogue input context, this will free the memory for the context.
   MraaReturnCode close(Pointer<MraaAioContext> dev) =>
       returnCode.fromInt(_closeFunc(dev));
 
   /// Set bit - mraa_aio_set_bit
-  /// Set the bit value which mraa will shift the raw reading from the ADC to. I.e. 10bits
+  ///
+  /// Set the bit value which MRAA will shift the raw reading from the ADC to, i.e. 10bits
   MraaReturnCode setBit(Pointer<MraaAioContext> dev, int bits) =>
       returnCode.fromInt(_setBitFunc(dev, bits));
 
   /// Set bit - mraa_aio_get_bit
-  /// Gets the bit value mraa is shifting the analog read to.
+  ///
+  /// Gets the bit value mraa is shifting the analogue read to.
   int getBit(Pointer<MraaAioContext> dev) => _getBitFunc(dev);
 
   void _setUpPointers() {
     _initPointer =
-        _lib.lookup<NativeFunction<returnMraaAioContextIntParameterFunc>>(
+        _lib.lookup<NativeFunction<_returnMraaAioContextIntParameterFunc>>(
             'mraa_aio_init');
     _readPointer =
-        _lib.lookup<NativeFunction<returnIntMraaAioContextParameterFunc>>(
+        _lib.lookup<NativeFunction<_returnIntMraaAioContextParameterFunc>>(
             'mraa_aio_read');
     _readDoublePointer =
-        _lib.lookup<NativeFunction<returnDoubleMraaAioContextParameterFunc>>(
+        _lib.lookup<NativeFunction<_returnFloatMraaAioContextParameterFunc>>(
             'mraa_aio_read_float');
     _closePointer =
-        _lib.lookup<NativeFunction<returnIntMraaAioContextParameterFunc>>(
+        _lib.lookup<NativeFunction<_returnIntMraaAioContextParameterFunc>>(
             'mraa_aio_close');
     _setBitPointer =
-        _lib.lookup<NativeFunction<returnIntMraaAioContextIntParameterFunc>>(
+        _lib.lookup<NativeFunction<_returnIntMraaAioContextIntParameterFunc>>(
             'mraa_aio_set_bit');
     _getBitPointer =
-        _lib.lookup<NativeFunction<returnIntMraaAioContextParameterFunc>>(
+        _lib.lookup<NativeFunction<_returnIntMraaAioContextParameterFunc>>(
             'mraa_aio_get_bit');
   }
 
   void _setUpFunctions() {
-    _initFunc = _initPointer.asFunction<MraaAioInitialiseType>();
-    _readFunc = _readPointer.asFunction<MraaAioReadType>();
-    _readDoubleFunc = _readDoublePointer.asFunction<MraaAioReadDoubleType>();
-    _closeFunc = _closePointer.asFunction<MraaAioCloseType>();
-    _setBitFunc = _setBitPointer.asFunction<MraaAioSetBitType>();
-    _getBitFunc = _getBitPointer.asFunction<MraaAioGetBitType>();
+    _initFunc = _initPointer.asFunction<_MraaAioInitialiseType>();
+    _readFunc = _readPointer.asFunction<_MraaAioReadType>();
+    _readDoubleFunc = _readDoublePointer.asFunction<_MraaAioReadDoubleType>();
+    _closeFunc = _closePointer.asFunction<_MraaAioCloseType>();
+    _setBitFunc = _setBitPointer.asFunction<_MraaAioSetBitType>();
+    _getBitFunc = _getBitPointer.asFunction<_MraaAioGetBitType>();
   }
 }
