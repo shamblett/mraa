56e85df152cdacd21a388aa3436682c8d71f6aad
Steve Hamblett <steve.hamblett@linux.com>
2021-04-03 13:02:58 +0100

    Unit test refactor

---
+++ test/mraa_aio_test.dart
@@ -0,0 +1,69 @@
+/*
+ * Package : mraa
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 03/10/2019
+ * Copyright :  S.Hamblett
+ */
+
+import 'package:mraa/mraa.dart';
+import 'package:test/test.dart';
+import 'support/mraa_platform_helper.dart';
+
+@TestOn('VM && Linux')
+
+// We always use our package supplied library in test as it is an Mraa
+// library compiled to use a mock board, this means that some values used
+// in the tests below are as supplied by the mock board implementation,
+// some devices however, e.g. PWM are not supported by the mock board
+// implementation and therefore cannot be tested. In all cases the API
+// call itself is exercised for correctness.
+int main() {
+  // Setup
+  final helper = MraaPlatformHelper();
+  print('OS is ${helper.osMap['NAME']}, lib path is ${helper.getTestLib()}');
+  final mraa = Mraa.fromLib(helper.getTestLib());
+  mraa.initialise();
+
+  // Test groups
+
+  group('AIO', () {
+    test('Initialise', () {
+      mraa.common.initialise();
+      final context = mraa.aio.initialise(0);
+      expect(context, isNotNull);
+    });
+    test('Close', () {
+      mraa.common.initialise();
+      final context = mraa.aio.initialise(0);
+      final ret = mraa.aio.close(context);
+      expect(ret, MraaReturnCode.success);
+    });
+    test('Read', () {
+      mraa.common.initialise();
+      final context = mraa.aio.initialise(0);
+      final val = mraa.aio.read(context);
+      expect(val.isNegative, isFalse);
+    });
+    test('Read double', () {
+      mraa.common.initialise();
+      final context = mraa.aio.initialise(0);
+      final val = mraa.aio.readDouble(context);
+      // For the unsupported test board used this value will be nonsense
+      expect(val < Mraa.aioReadDoubleError, isFalse);
+    });
+    test('Set bit', () {
+      mraa.common.initialise();
+      final context = mraa.aio.initialise(0);
+      final ret = mraa.aio.setBit(context, 10);
+      expect(ret, MraaReturnCode.success);
+    });
+    test('Get bit', () {
+      mraa.common.initialise();
+      final context = mraa.aio.initialise(0);
+      final ret = mraa.aio.getBit(context);
+      expect(ret, 10);
+    });
+  });
+
+  return 0;
+}
---
+++ test/mraa_led_test.dart
@@ -0,0 +1,77 @@
+/*
+ * Package : mraa
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 03/10/2019
+ * Copyright :  S.Hamblett
+ */
+
+import 'package:mraa/mraa.dart';
+import 'package:test/test.dart';
+import 'support/mraa_platform_helper.dart';
+
+@TestOn('VM && Linux')
+
+// We always use our package supplied library in test as it is an Mraa
+// library compiled to use a mock board, this means that some values used
+// in the tests below are as supplied by the mock board implementation,
+// some devices however, e.g. PWM are not supported by the mock board
+// implementation and therefore cannot be tested. In all cases the API
+// call itself is exercised for correctness.
+int main() {
+  // Setup
+  final helper = MraaPlatformHelper();
+  print('OS is ${helper.osMap['NAME']}, lib path is ${helper.getTestLib()}');
+  final mraa = Mraa.fromLib(helper.getTestLib());
+  mraa.initialise();
+
+  // Test groups
+
+  group('LED', () {
+    test('Initialise', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+    });
+    test('Initialise raw', () {
+      final context = mraa.led.initialiseRaw('user4');
+      expect(context, isNotNull);
+    });
+    test('Set brightness', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+      final ret = mraa.led.setBrightness(context, 10);
+      expect(ret, MraaReturnCode.errorInvalidHandle);
+    });
+    test('Read brightness', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+      final ret = mraa.led.readBrightness(context);
+      expect(ret, 5);
+    });
+    test('Read maximum brightness', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+      final ret = mraa.led.readMaxBrightness(context);
+      expect(ret, 5);
+    });
+    test('Set trigger', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+      final ret = mraa.led.setTrigger(context, 'heartbeat');
+      expect(ret, MraaReturnCode.errorInvalidHandle);
+    });
+    test('Clear trigger', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+      final ret = mraa.led.clearTrigger(context);
+      expect(ret, MraaReturnCode.errorInvalidHandle);
+    });
+    test('Close', () {
+      final context = mraa.led.initialise(1);
+      expect(context, isNotNull);
+      final ret = mraa.led.close(context);
+      expect(ret, MraaReturnCode.errorInvalidHandle);
+    });
+  });
+
+  return 0;
+}
